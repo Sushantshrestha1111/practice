4898c04d037824a87333de2af995f8f9bfe554d1
SushantShrestha <shresthasushant11@gmail.com>
2024-10-01 17:37:57 +0545

    first day of vuejs

---
+++ vuejs/Ambition/to-do/src/components/Conditional_Rendering.vue
@@ -0,0 +1,60 @@
+<template>
+    <h1 v-if="number===5">number is {{ number }}</h1>
+    <h1 v-else-if="number <0">The number is negetive</h1>
+    <h1 v-else-if="number >0">The number is positive</h1>
+    <h1 v-else>is not a number</h1>
+    <template v-if="display">
+      <h2>Sushatn</h2>
+      <h2 v-html="nickname"></h2>
+    </template>
+    <h1 v-show="showElement">v- show element</h1>
+   
+  </template>
+   
+  <script>
+  // "import BackgroundCardVue from "./components/Background-card.vue";"
+  export default {
+    name: 'App',
+    // components: {
+    //   BackgroundCardVue,
+    // },
+    data() {
+      return {
+        number:5,
+        display:true,
+        nickname:'<u>Ghost</u>',
+        showElement:true,
+        
+      };
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .underline{
+    text-decoration: underline;
+  
+  }
+  .promoted{
+    font-style: italic; 
+  }
+  .sold-out{
+    color: red;
+  
+  }
+  
+  .new{
+    color: green;
+  
+  }
+  </style>
+  
