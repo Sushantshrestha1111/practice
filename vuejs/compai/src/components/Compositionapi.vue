8a18864d5eb112f8d1870988b74da732a6966bc0
Sushant <shresthasushant11@gmail.com>
2024-10-12 18:20:28 +0545

    compapi

---
+++ vuejs/compai/src/components/Compositionapi.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div>
+        <h1>This is the composition api</h1>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"CompositionApi",
+    
+}
+</script>
+<style lang="">
+    
+</style>
